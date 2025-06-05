8fcce04b09e9b2ec6c9b6f61e6f8b246901294b3
Matias Fanucchi <matiasfanucchi99@gmail.com>
2025-06-05 11:18:24 -0300

    ejercicio 3 completo

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -2,14 +2,37 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+
+//Maximizar el número de actividades compatibles. Se tienen n actividades que necesitan utilizar un
+//recurso, tal como una sala de conferencias, en exclusión mutua. Cada actividad i tiene asociado un
+//tiempo de comienzo ci y un tiempo de finalización fi de utilización del recurso, con ci < fi. Si la
+//actividad i es seleccionada se llevará a cabo durante el intervalo [ci, fi). Las actividades i y j son
+//compatibles si los intervalos [ci, fi) y [cj, fj) no se superponen (es decir, ci > fj o cj > fi). El problema
+//consiste en encontrar la cantidad máxima de actividades compatibles entre sí.
 public class Ejercicio3 {
 
-    class Actividad {
-        int comienzo;
-        int fin;
+    public class Actividad {
+        private int comienzo;
+        private int fin;
 
         public Actividad(int comienzo, int fin) {
             this.comienzo = comienzo;
+            this.fin = fin;
+        }
+
+        public int getComienzo() {
+            return comienzo;
+        }
+
+        public void setComienzo(int comienzo) {
+            this.comienzo = comienzo;
+        }
+
+        public int getFin() {
+            return fin;
+        }
+
+        public void setFin(int fin) {
             this.fin = fin;
         }
     }
@@ -25,9 +48,9 @@ public class Ejercicio3 {
             int tiempoFinAnterior = 0;
 
             for (Actividad a : actividades) {
-                if (a.comienzo >= tiempoFinAnterior) {
+                if (a.getComienzo() >= tiempoFinAnterior) {
                     seleccionadas.add(a);
-                    tiempoFinAnterior = a.fin;
+                    tiempoFinAnterior = a.getFin();
                 }
             }
 
